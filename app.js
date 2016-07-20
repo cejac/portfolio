4c0643c6384d85f1b5895038b88c6c99184ec777
Carolina Vasquez-Ceja <cejac10@gmail.com>
2016-07-19 23:56:35 -0700

    added basic outline, need to complete

---
+++ app.js
@@ -0,0 +1,12 @@
+var projects = [];
+
+function Project(opts){
+  this.title = opts.title;
+  this.url = opts.url;
+  this.published = opts.published;
+  this.about = opts.about;
+}
+
+Projects.prototype.toHTML = function(){
+
+};
---
+++ projects.js
@@ -0,0 +1,8 @@
+var data = [
+  {
+    title:      'About Me',
+    url:        'https://github.com/cejac/about-me',
+    published:  '2016-06-04',
+    about:      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis turpis lorem, egestas quis elementum eu, venenatis a orci. Nulla pulvinar nunc eleifend odio dictum ullamcorper. Donec vehicula finibus maximus. Nunc scelerisque diam nisl, nec sollicitudin diam malesuada eu. Vivamus in eleifend erat, sed lobortis sem.',
+  },
+];
