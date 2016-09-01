fc04207f11b980b3bf0ef7bface41a0834ad219d
Carolina Vasquez-Ceja <cejac10@gmail.com>
2016-08-31 18:28:09 -0700

    working on class14

--- scripts/projectsController.js
+++ scripts/projectsController.js
@@ -5,11 +5,25 @@
   Project.getAll(projectView.initPage);
   console.log('sup');
 
-  projectController.index = function(){
+  projectController.index = function(ctx, next){
+    //projectView.index goes here. Need to make.
+
+
     console.log('hi');
     $('.tab-content').hide();
     $('#project-section').show();
   };
 
+  articlesController.loadByTitle = function(ctx, next) {
+    //
+
+  };
+
+  articlesController.loadAll = function(ctx, next) {
+    //
+  };
+
+  //need to figure out how this goes with routes, etc.
+  
   module.projectController = projectController;
 })(window);
